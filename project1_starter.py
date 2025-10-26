94dda9c052226535ea916c07ac34bcc292a6b08b
Samuel Somerville <sam.mikesomerville@gmail.com>
2025-10-26 19:22:36 -0400

    Update project1_starter.py

--- project1_starter.py
+++ project1_starter.py
@@ -88,23 +88,8 @@ def save_character(character, filename):
             file.write(f"Magic: {character["magic"]}\n")
             file.write(f"Health: {character["health"]}\n")
             file.write(f"Gold: {character["gold"]}")
-            return True
+            return "f"
     else:
-        if "/" in filename:
-            os.path.join(filename.split("/"))
-            if os.path.exists(filename):
-                with open(filename, "w") as file:
-                    file.write(f"Character Name: {character["name"]}\n")
-                    file.write(f"Class: {character["class"]}\n")
-                    file.write(f"Level: {character["level"]}\n")
-                    file.write(f"Strength: {character["strength"]}\n")
-                    file.write(f"Magic: {character["magic"]}\n")
-                    file.write(f"Health: {character["health"]}\n")
-                    file.write(f"Gold: {character["gold"]}")
-                    return True
-            else:
-                return False
-        else:
             return False
 
 
